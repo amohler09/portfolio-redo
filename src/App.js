f3971824a55c961636d1c13f6569a8a721fe08ad
Amber Mohler <ambermohler@Ambers-MacBook-Pro.local>
2020-09-26 18:07:26 -0400

    adds react-router with route paths

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import HomePage from './pages/HomePage'
+import PastProjects from './pages/PastProjects'
+import ResumePage from './pages/ResumePage'
 import './App.css';
+
+import { Route } from 'react-router-dom'
 
 function App() {
   
   return (
     <div>
-      Hello!
+      <Route exact path='/'>
+      <HomePage />
+      </Route>
+      <Route path='/PastProjects'>
+      <PastProjects />
+</Route>
+<Route path='/Resume'>
+<ResumePage />
+</Route>
+      
+      
+      
     </div>  );
 }
 
