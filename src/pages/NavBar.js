5230e5c591c15526a88c078e4fab9285eca64259
Amber Mohler <ambermohler@Ambers-MacBook-Pro.local>
2020-10-30 13:44:44 -0400

    adds NavBar.js and styling

---
+++ src/pages/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './NavBar.scss'
+
+const ProfilePicture = require('../img/ProfilePicture.JPG')
+
+export default function NavBar() {
+  return (
+    <nav>
+      <img src={ProfilePicture} />
+      <Link to='/' className='navLink'>About Me</Link>
+      <Link to='Resume' className='navLink'>Resume</Link>
+      <Link to='PastProjects' className='navLink'>Past Projects</Link>
+      <Link to='Contact' className='navLink'>Contact</Link>
+      
+    </nav>
+  )
+}
