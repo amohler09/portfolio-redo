5a59216c7f7fe42fe2b5737e44ceaf27d334bce5
Amber Mohler <ambermohler@Ambers-MacBook-Pro.local>
2020-10-30 13:44:29 -0400

    adds routes and basic structure to app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React from 'react';
+import NavBar from './pages/NavBar'
 import HomePage from './pages/HomePage'
 import PastProjects from './pages/PastProjects'
 import ResumePage from './pages/ResumePage'
-import './App.css';
+import Contact from './pages/Contact'
+import './App.scss';
 
 import { Route } from 'react-router-dom'
 
 function App() {
   
   return (
-    <div>
+    <main>
+      <div className='navbar'>
+        <NavBar />
+      </div>
       <Route exact path='/'>
-      <HomePage />
+        <HomePage />
       </Route>
       <Route path='/PastProjects'>
-      <PastProjects />
-</Route>
-<Route path='/Resume'>
-<ResumePage />
-</Route>
-      
-      
-      
-    </div>  );
+        <PastProjects />
+      </Route>
+      <Route path='/Resume'>
+        <ResumePage />
+      </Route>
+      <Route path='/Contact'>
+        <Contact />
+      </Route>
+    </main>  );
 }
 
 export default App;
