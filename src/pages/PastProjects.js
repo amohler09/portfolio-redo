c5b6d01b4042ed37bf26a951f56e914be6ab18b8
Amber Mohler <ambermohler@Ambers-MacBook-Pro.local>
2020-10-31 15:12:59 -0400

    adds initial structure for PastProjects.js

--- src/pages/PastProjects.js
+++ src/pages/PastProjects.js
@@ -1,9 +1,71 @@
 import React from 'react'
+import './PastProjects.scss'
+
+const Placeholder = require('../img/ProfilePicture.JPG')
 
 export default function PastProjects() {
   return (
-    <div>
-      Past Projects
+    <div className='project-cont'>
+      <ul className='projects clr'>
+  <li class="pusher"></li>
+	<li>
+      <div>
+        <img src={Placeholder} alt=""/>
+        <h2>Project Title</h2>
+    </div>
+  </li>
+     <li>
+      <div>
+      <img src={Placeholder} alt=""/>
+        <h2>Project Title</h2>
+    </div>
+  </li>
+  <li>
+      <div>
+      <img src={Placeholder} alt=""/>
+        <h2>Project Title</h2>
+    </div>
+  </li>
+<li>
+      <div>
+        <img src={Placeholder} alt=""/>
+        <h2>Project Title</h2>
+    </div>
+  </li>
+     <li class="pusher"></li>
+  <li>
+      <div>
+      <img src={Placeholder} alt=""/>
+        <h2>Project Title</h2>
+    </div>
+  </li>
+  
+  <li>
+      <div>
+      <img src={Placeholder} alt=""/>
+        <h2>Project Title</h2>
+    </div>
+  </li>
+  <li class="pusher"></li>
+  <li>
+      <div>
+      <img src={Placeholder} alt=""/>
+        <h2>Project Title</h2>
+    </div>
+  </li>
+  <li>
+      <div>
+      <img src={Placeholder} alt=""/>
+        <h2>Project Title</h2>
+    </div>
+  </li><li>
+      <div>
+      <img src={Placeholder} alt=""/>
+        <h2>Project Title</h2>
+    </div>
+  </li>
+</ul>
+      {/* <p>The inspiration for this page design came from <a href='https://codepen.io/amohler/pen/qBNYWGZ?editors=1100'>this codepen</a>. */}
     </div>
   )
 }
