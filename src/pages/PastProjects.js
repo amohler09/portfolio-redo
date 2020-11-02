ef07baf3fe07c71a5aa5f61dbe00fb6fe1559d61
Amber Mohler <ambermohler@Ambers-MacBook-Pro.local>
2020-11-01 23:43:00 -0500

    adds toggle function to show project details

--- src/pages/PastProjects.js
+++ src/pages/PastProjects.js
@@ -1,71 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './PastProjects.scss'
 
 const Placeholder = require('../img/ProfilePicture.JPG')
 
 export default function PastProjects() {
-  return (
-    <div className='project-cont'>
-      <ul className='projects clr'>
-  <li class="pusher"></li>
-	<li>
-      <div>
-        <img src={Placeholder} alt=""/>
-        <h2>Project Title</h2>
-    </div>
-  </li>
-     <li>
-      <div>
-      <img src={Placeholder} alt=""/>
-        <h2>Project Title</h2>
-    </div>
-  </li>
-  <li>
-      <div>
-      <img src={Placeholder} alt=""/>
-        <h2>Project Title</h2>
-    </div>
-  </li>
-<li>
-      <div>
-        <img src={Placeholder} alt=""/>
-        <h2>Project Title</h2>
-    </div>
-  </li>
-     <li class="pusher"></li>
-  <li>
-      <div>
-      <img src={Placeholder} alt=""/>
-        <h2>Project Title</h2>
-    </div>
-  </li>
+  const [showDetails, setShowDetails] = useState(0);
+
+  const detailToggle = project => () => showDetails === project ? setShowDetails(false) : setShowDetails(project);
   
-  <li>
-      <div>
-      <img src={Placeholder} alt=""/>
-        <h2>Project Title</h2>
-    </div>
-  </li>
-  <li class="pusher"></li>
-  <li>
-      <div>
-      <img src={Placeholder} alt=""/>
-        <h2>Project Title</h2>
-    </div>
-  </li>
-  <li>
-      <div>
-      <img src={Placeholder} alt=""/>
-        <h2>Project Title</h2>
-    </div>
-  </li><li>
-      <div>
-      <img src={Placeholder} alt=""/>
-        <h2>Project Title</h2>
-    </div>
-  </li>
-</ul>
-      {/* <p>The inspiration for this page design came from <a href='https://codepen.io/amohler/pen/qBNYWGZ?editors=1100'>this codepen</a>. */}
+  return (
+    <div>
+      <ul className='projects'>
+        <li className={showDetails === 1 ? 'details' : ''}>Lee McGhee Roofing</li>
+        <li>
+          <div onClick={detailToggle(1)}>
+            <img src={Placeholder} alt=""/>
+            <h2>Lee McGhee Roofing</h2>
+          </div>
+        </li>
+        <li>
+          <div onClick={detailToggle(2)}>
+            <img src={Placeholder} alt=""/>
+            <h2>Lambda School Project Adaptiv Apps - Chat Feature</h2>
+          </div>
+        </li>
+        <li className={showDetails === 2 ? 'details' : ''}>Lambda School Project Adaptiv Apps - Chat Feature</li>
+        <li className={showDetails === 3 ? 'details' : ''}>Adaptiv Apps - Freelance</li>
+        <li>
+          <div onClick={detailToggle(3)}>
+            <img src={Placeholder} alt=""/>
+            <h2>Adaptiv Apps - Freelance</h2>
+          </div>
+        </li>
+        <li>
+          <div onClick={detailToggle(4)}>
+            <img src={Placeholder} alt=""/>
+            <h2>Connie's Cookbook</h2>
+          </div>
+        </li>
+        <li className={showDetails === 4 ? 'details' : ''}>Connie's Cookbook</li>
+        <li className={showDetails === 5 ? 'details' : ''}>The World's Most Impressive To-do List</li>
+        <li>
+          <div onClick={detailToggle(5)}>
+            <img src={Placeholder} alt=""/>
+            <h2>The World's Most Impressive To-do List</h2>
+          </div>
+        </li>
+        </ul>
+      {/* <p>The inspiration for this page design came from <a href='https://codepen.io/amohler/pen/qBNYWGZ?editors=1100'>this codepen</a>.*/}
     </div>
   )
 }
