4e379a4b974bea1a1ff35730276269876105ce8d
Amber Mohler <ambermohler@Ambers-MacBook-Pro.local>
2020-12-07 14:20:51 -0500

    adds styling, media queries to PastProjects.js

--- src/pages/PastProjects.js
+++ src/pages/PastProjects.js
@@ -12,40 +12,40 @@ export default function PastProjects() {
     {
       imgUrl: require('../img/ProfilePicture.JPG'),
       title: `Lee McGhee Roofing`,
-      summary: 'summary'
+      summary: 'summarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummary'
     },
     {
       imgUrl: require('../img/ProfilePicture.JPG'),
       title: `Lambda School Labs Project - Adaptiv Apps - Chat Feature`,
-      summary: 'summary'
+      summary: 'summarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummary'
     },
     {
       imgUrl: require('../img/ProfilePicture.JPG'),
       title: `Adaptiv Apps - Freelance`,
-      summary: 'summary'
+      summary: 'summarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummary'
     },
     {
       imgUrl: require('../img/ProfilePicture.JPG'),
       title: `Connie's Cookbook`,
-      summary: 'summary'
+      summary: 'summarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummary'
     },
     {
       imgUrl: require('../img/ProfilePicture.JPG'),
       title: `The World's Most Impressive To-do List`,
-      summary: 'summary'
+      summary: 'summarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummarysummary'
     },
   ]
   
   return (
     <div className='projects'>
        {Projects.map((project, i) =>  (
-          <div className={i % 2 ? 'flex' : 'flex reverse'}>
+          <div className={i % 2 ? 'flex' : 'flex reverse'} style={showDetails === i ? {padding: '0'} : null}>
             <div 
               className='project-cont' onClick={detailToggle(i)}>
               <img src={project.imgUrl} alt='' />
-              <h2 className='title'>{project.title}</h2>
+              <h2 className='title'>{showDetails !== i && project.title}</h2>
             </div>
-            <p className={showDetails === i ? 'details' : 'detail-hide' }>{project.summary}</p>
+            <p onClick={detailToggle(i)} className={showDetails === i ? 'details' : 'detail-hide' }>{project.summary}</p>
           </div>
         ))}
      </div>
