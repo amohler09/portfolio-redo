6d7965046fab2f318deb63ed542cd54b2a5b46d8
Amber Mohler <ambermohler@Ambers-MacBook-Pro.local>
2020-12-09 15:19:21 -0500

    adds emailJS dependency & contact form with successful API calls

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Contact.scss'
+import emailjs from 'emailjs-com';
 
 export default function Contact() {
+  const [formData, setFormData] = useState({
+    from_name: '',
+    from_email: '',
+    from_phone: '',
+    message: '',
+  });
+
+  const template = process.env.REACT_APP_EMAILJS_TEMPLATEID;
+  const userID = process.env.REACT_APP_EMAILJS_USERID;
+
+  const handleChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  console.log('template', template)
+  console.log(formData);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    emailjs.send('Contact_Form', template, formData, userID)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }
+
   return (
     <div className='contact-page'>
       <h1>Contact</h1>
@@ -22,7 +51,41 @@ export default function Contact() {
           <p>Tampa, FL</p>
         </div>
       </div>
-      Contact Form
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name
+        </label>
+        <input
+          name='from_name' 
+          value={formData.from_name}
+          onChange={handleChange}
+        />
+        <label>
+          Email
+        </label>
+        <input
+          name='from_email' 
+          value={formData.from_email}
+          onChange={handleChange}
+        />
+        <label>
+          Phone Number
+        </label>
+        <input
+          name='from_phone' 
+          value={formData.from_phone}
+          onChange={handleChange}
+        />
+        <label>
+          Message
+        </label>
+        <input
+          name='message' 
+          value={formData.from_message}
+          onChange={handleChange}
+        />
+        <button type='submit'>Send</button>
+      </form>
     </div>
   )
 }
