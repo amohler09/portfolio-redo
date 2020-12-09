f5637d59ff6b79a7abc5cc5d14d931520c4389a8
Amber Mohler <ambermohler@Ambers-MacBook-Pro.local>
2020-12-09 14:23:23 -0500

    adds content & styling to Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import './Contact.scss'
 
 export default function Contact() {
   return (
-    <div>
-      Contact
+    <div className='contact-page'>
+      <h1>Contact</h1>
+      <div className='flex-cont' >
+        <div className='contact'>
+          <i class="fas fa-globe-americas"></i>
+          <h3>Current Location</h3>
+          <p>Tampa, FL</p>
+        </div>
+        <div className='contact'>
+          <i class="fas fa-mobile-alt"></i>
+          <h3>Phone</h3>
+          <p>Tampa, FL</p>
+        </div>
+        <div className='contact'>
+          <i class="far fa-paper-plane"></i>
+          <h3>Email</h3>
+          <p>Tampa, FL</p>
+        </div>
+      </div>
+      Contact Form
     </div>
   )
 }
+
+
